35b785e30bcc685ddfd7e443c429bc71b42b7664
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-02-13 22:21:13 -0500

    added set position x,y method

--- gameObj.h
+++ gameObj.h
@@ -19,35 +19,13 @@ class gameObj {
 	public:
 
 	std::string textureString;
-
-	int lastMove;
-
-	// fire rate members
-	int duration = 0; // duration of old bullet on screen
-	int timeout = 0; // time before bullet shoul be fired
-
 	SDL_Rect rect; // obj rect (used for coordinates)
+
 	double velocity = 1;
-	double velocityMod = 1;
 
 	// starting position
 	int initialX = 0;
 	int initialY = 0;
-
-	// sequence of coords and moves
-	// ((x,y), move)
-	std::list<std::pair<std::pair<int, int>, int>> moveSeq;
-
-	// default constructor
-	gameObj();
-
-	// detailed constructor
-// takes texture string, velocity, width, height, xPos, yPos
-	gameObj(std::string t, const double& vel, const int &width, const int &height, const int &xPos = 0 ,const int &yPos = 0);
-
-	// copy constructor with rect coords
-	// takes rhs gameObj, xPos, yPos
-	gameObj(const gameObj& other, const int &xPos, const int &yPos);
 
 	bool isOffscreen() const;
 
@@ -61,6 +39,35 @@ class gameObj {
 		initialX = x;
 		initialY = y;
 	}
+
+	void setPos(const int& x, const int& y)
+	{
+		rect.x = x;
+		rect.y = y;
+	}
+
+	// default constructor
+	gameObj();
+
+	// detailed constructor
+	// takes texture string, velocity, width, height, xPos, yPos
+	gameObj(std::string t, const double& vel, const int &width, const int &height, const int &xPos = 0 ,const int &yPos = 0);
+
+	// copy constructor with rect coords
+	// takes rhs gameObj, xPos, yPos
+	gameObj(const gameObj& other, const int &xPos, const int &yPos);
+
+	int lastMove;
+
+	// fire rate members
+	int duration = 0; // duration of old bullet on screen
+	int timeout = 0; // time before bullet shoul be fired
+
+	double velocityMod = 1;
+
+	// sequence of coords and moves
+	// ((x,y), move)
+	std::list<std::pair<std::pair<int, int>, int>> moveSeq;
 
 	void resetTimeout() { timeout = duration + SDL_GetTicks(); }
 
